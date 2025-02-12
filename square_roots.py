47aed369451143afcfb378bef6de8d35003a2f5b
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2025-02-12 04:10:09 +0000

    bob

--- square_roots.py
+++ square_roots.py
@@ -1,1 +1,13 @@
-
+import random
+basenum=int(input("Bottom num to study"))
+squares=[]
+for i in range (basenum, 26) :
+    squares.append(i)
+random.shuffle(squares)
+for x in squares:
+    responce=int(input("What is the square root"+str (x*x) +"?"))
+    answer=x
+if responce==answer:
+    print("Correct")
+else:
+    print("Incorrect")
