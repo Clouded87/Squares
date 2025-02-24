765886596c368a20293c8d01d5ee4dbdc9c20bb1
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2025-02-24 14:54:41 +0000

    bob

--- merge.py
+++ merge.py
@@ -9,26 +9,38 @@ for i in range(basenum, 26):
 for i in range(basenum+25, 51):
     squares.append(i)
 random.shuffle(squares)
-for x in squares:
-    if x>25:
-        x=x-25
-        responce=int(input("What is the square root of "+str (x*x) +"?"))
-        answer=x
-        if responce==answer:
-            print("Correct")
-            correct.append(x*x)
+def practice():
+    global squares, correct, incorrect, incorrect_internal
+    for x in squares:
+        if x>25:
+            x=x-25
+            responce=int(input("What is the square root of "+str (x*x) +"?"))
+            answer=x
+            if responce==answer:
+                print("Correct")
+                correct.append("Square root of "+str(x*x))
+            else:
+                print("Incorrect it is "+str(answer))
+                incorrect_internal.append(x+25)
+                incorrect.append("Square root of "+str(x*x))
         else:
-            print("Incorrect it is "+str(answer))
-            incorrect_internal.append(x+25)
-            incorrect.append(x*x)
+            responce=int(input("What is "+str(x)+" squared?"))
+            answer=int(x)*int(x)
+            if responce==answer:
+                print("Correct")
+                correct.append(str(x)+" squared")
+            else:
+                print("Incorrect it is "+str(answer))
+                incorrect.append(str(x)+" squared")
+                incorrect_internal.append(x)
+    print(incorrect)
+    quit=input("Do you want to continue with incorrect answers?(y/n)")
+    if quit=="y":
+        squares=incorrect_internal
+        incorrect=[]
+        incorrect_internal=[]
+        correct=[]
+        practice()
     else:
-        responce=int(input("What is "+str(x)+" squared?"))
-        answer=int(x)*int(x)
-        if responce==answer:
-            print("Correct")
-            correct.append(x)
-        else:
-            print("Incorrect it is "+str(answer))
-            incorrect.append(x)
-            incorrect_internal.append(x)
-print(incorrect_internal)
+        pass
+practice()
