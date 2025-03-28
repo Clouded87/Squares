b487bddaaf9a6217995cd9eb7474e77754c32c78
TheArchmage87 <144473122+TheArchmage87@users.noreply.github.com>
2025-03-28 13:38:12 +0000

    bob

--- pythag.py
+++ pythag.py
@@ -5,12 +5,18 @@ set1="3-4-5"
 set2="1-1√3-2"
 set3="1-1-1√2"
 setlist=[set1, set2, set3]
+newitem=0
 if yesorno==0:
     set=random.randint(0, 2)
     chosenset=setlist[set]
     chosenset=chosenset.split("-")
     multiple=random.randint(1, 15)
-    for i in range(chosenset):
-        newitem=str(int(chosenset[i])*multiple)
+    for i in chosenset:
+        newset=i.split("√")
+        if len(newset)==2:
+            newitem=str(int(newset[0]*multiple))+"√"+newset[1]
+        elif len(newset)==1:
+            newitem=str(int(newset[0])*multiple)
         finalset.append(newitem)
     print(chosenset)
+    print(finalset)
